4f5629a96f19c7773fd6a3723114afc677e9ed4e
UkiOnna <sikirovtemirlan@mail.ru>
2018-06-06 17:46:58 +0600

    mail

---
+++ MailService/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace MailService
+{
+    class Program
+    {
+        private const string BASE_EMAIL = "smtptesteritstep@gmail.com";
+        private const string BASE_EMAIL_PASSWORD = "167AEq!!";
+        static void Main(string[] args)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.Credentials = new NetworkCredential(BASE_EMAIL, BASE_EMAIL_PASSWORD);
+
+            MailMessage message = new MailMessage();
+            message.From=new MailAddress(BASE_EMAIL,"Eduard");
+            message.To.Add(new MailAddress("sikirovtemirlan@mail.ru"));
+            message.Subject = "Гони пятихатку";
+            message.Body = "Пацан проценты капают братаны уже выехали";
+            message.Attachments.Add(new Attachment(@"C:\Users\СикировТ\Desktop\ControlOrder.txt"));
+
+            smtpClient.Send(message);
+        }
+    }
+}
